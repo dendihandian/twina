3a31fd8ddf3c4258f4dfc430c3fa1e015000c341
Dendi Handian <handian.dendi@gmail.com>
2020-09-08 22:27:37 +0700

    add result type to table

--- resources/views/public/topics/index.blade.php
+++ resources/views/public/topics/index.blade.php
@@ -15,22 +15,27 @@
                     <table class="table">
                         <thead class="thead-dark">
                             <tr>
-                                <th scope="col">Topic</th>
-                                <th scope="col" width="5%">Tweet Count</th>
-                                <th scope="col" class="text-center">Last Fetch Tweet</th>
-                                <th scope="col" width="5%">Last Fetch Count</th>
-                                <th scope="col">Last Fetch Date</th>
-                                <th scope="col">Created At</th>
-                                <th scope="col" class="text-center">On Queue</th>
-                                <th scope="col" class="text-center" width="20%">Actions</th>
+                                <th scope="col" rowspan="2" class="align-middle">Topic</th>
+                                <th scope="col" rowspan="2" class="align-middle">Result Type</th>
+                                <th scope="col" rowspan="2" class="align-middle">Tweet Count</th>
+                                <th scope="col" colspan="3" class="align-middle text-center">Last Fetch</th>
+                                <th scope="col" rowspan="2" class="align-middle">Created At</th>
+                                <th scope="col" rowspan="2" class="align-middle text-center">On Queue</th>
+                                <th scope="col" rowspan="2" class="align-middle text-center" width="20%">Actions</th>
+                            </tr>
+                            <tr>
+                                <th scope="col" class="align-middle">Tweet</th>
+                                <th scope="col" class="align-middle">Count</th>
+                                <th scope="col" class="align-middle">Date</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($topics as $topicId => $topic)
                                 <tr>
-                                    <th scope="row">{{ $topic['text'] ?? ''}}</th>
-                                    <td class="text-right">{{ (isset($topic['tweets_count']) && !empty($topic['tweets_count'])) ? $topic['tweets_count'] : '-' }}</td>
-                                    <td class="text-center">
+                                    <th scope="row" class="align-middle">{{ $topic['text'] ?? ''}}</th>
+                                    <th scope="row" class="align-middle">{{ $topic['result_type'] ?? 'recent'}}</th>
+                                    <td class="align-middle text-right">{{ (isset($topic['tweets_count']) && !empty($topic['tweets_count'])) ? $topic['tweets_count'] : '-' }}</td>
+                                    <td class="align-middle text-center">
                                         @if ($topic['last_fetch_tweet'])
                                             <a href="{{ 'https://twitter.com/' . $topic['last_fetch_tweet']['user']['screen_name'] . '/status/' . $topic['last_fetch_tweet']['id'] }}" target="_blank">
                                                 {{ (isset($topic['last_fetch_tweet']) && !empty($topic['last_fetch_tweet'])) ? $topic['last_fetch_tweet']['id'] : '-' }}
@@ -39,10 +44,10 @@
                                             {{ $tweet['id'] ?? '-' }}
                                         @endif
                                     </td>
-                                    <td class="text-right">{{ (isset($topic['last_fetch_count']) && !empty($topic['last_fetch_count'])) ? $topic['last_fetch_count'] : '-' }}</td>
-                                    <td>{{ (isset($topic['last_fetch_date']) && !empty($topic['last_fetch_date'])) ? $topic['last_fetch_date'] : '-' }}</td>
-                                    <td>{{ (isset($topic['created_at']) && !empty($topic['created_at'])) ? $topic['created_at'] : '-' }}</td>
-                                    <td class="text-center">{{ $topic['on_queue'] ? 'Yes' : 'No' }}</td>
+                                    <td class="align-middle text-right">{{ (isset($topic['last_fetch_count']) && !empty($topic['last_fetch_count'])) ? $topic['last_fetch_count'] : '-' }}</td>
+                                    <td class="align-middle">{{ (isset($topic['last_fetch_date']) && !empty($topic['last_fetch_date'])) ? $topic['last_fetch_date'] : '-' }}</td>
+                                    <td class="align-middle">{{ (isset($topic['created_at']) && !empty($topic['created_at'])) ? $topic['created_at'] : '-' }}</td>
+                                    <td class="align-middle text-center">{{ $topic['on_queue'] ? 'Yes' : 'No' }}</td>
                                     <td class="d-flex align-items-center justify-content-between">
                                         @if (!$topic['on_queue'])
                                             <form action="{{ route('public.topics.mining', ['topic' => $topicId]) }}" method="POST">
