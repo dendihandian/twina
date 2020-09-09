a38d0c91a9fbd5177b75e335576d90ac8f69cc5c
Dendi Handian <handian.dendi@gmail.com>
2020-09-10 05:14:37 +0700

    Home graph

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
+use App\Repositories\TopicRepository;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    protected $topicRepository;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(TopicRepository $topicRepository)
     {
+        $this->topicRepository = $topicRepository;
         $this->middleware('auth');
     }
 
@@ -25,6 +30,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $topic = $this->topicRepository->getSelectedTopic(Auth::user()->id);
+        $graph = $topic['graph'] ?? [];
+        if ($graph) {
+            $graph = [
+                'nodes' => array_values($graph['nodes']),
+                'links' => array_values($graph['edges']),
+            ];
+        }
+        return view('home', compact('topic', 'graph'));
     }
 }
