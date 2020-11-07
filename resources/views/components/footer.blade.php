3976d39ad8bd48e8f50c3e6e0f2692b835168083
Dendi Handian <handian.dendi@gmail.com>
2020-11-08 01:08:11 +0700

    change header and footer background

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,3 @@
-<footer class="py-2 px-4 bg-dark text-secondary d-flex justify-content-center justify-content-sm-end">
-    <span>
-        Twina &copy; 2020 Dendi Handian
-    </span>
+<footer class="py-2 px-4 bg-teal text-white d-flex justify-content-center justify-content-sm-end">
+    <span>Twina &copy; 2020 Dendi Handian</span>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,23 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-lg bg-teal py-2 px-4  py-lg-0">
+        <a class="navbar-brand text-white" href="#">
             <i class="fab fa-twitter"></i>&nbsp;Twina
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon text-white"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="{{ route('highlighted') }}" class="nav-link">Highlighted Topic</a></li>
-                <li class="nav-item"><a href="{{ route('public_topics') }}" class="nav-link">Public Topics</a></li>
+                <li class="nav-item nav-item-teal py-2 px-1"><a href="{{ route('highlighted') }}" class="nav-link text-white">Highlighted Topic</a></li>
+                <li class="nav-item nav-item-teal py-2 px-1"><a href="{{ route('public_topics') }}" class="nav-link text-white">Public Topics</a></li>
             </ul>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Login</a>
+                <li class="nav-item nav-item-teal py-2 px-1">
+                    <a href="#" class="nav-link text-white">Login</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Register</a>
+                <li class="nav-item nav-item-teal py-2 px-1">
+                    <a href="#" class="nav-link text-white">Register</a>
                 </li>
             </ul>
         </div>
