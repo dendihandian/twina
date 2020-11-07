ea0a73c288dae8cf14282c7fd7421d5d3c430b10
Dendi Handian <handian.dendi@gmail.com>
2020-11-08 00:01:27 +0700

    fix responsive landing page

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="landing-page-wrapper">
         <div class="jumbotron bg-light row d-flex align-items-center">
-            <div class="col-12 col-sm-6">
+            <div class="col-12 col-md-6 text-center text-md-left">
                 <h1>{{ __('Visualize Twitter Topics with Graph.') }}</h1>
                 <hr class="my-4">
                 <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus soluta labore velit esse itaque accusantium, eaque beatae corporis ullam, sapiente doloremque nemo vel architecto quidem iure ut fuga minima quia.</p>
-                <button class="btn btn-primary bg-teal border-0">{{ __('See the highlighted topic') }}</button>
+                <button class="mt-4 btn btn-primary bg-teal border-0">{{ __('See the highlighted topic') }}</button>
             </div>
-            <div class="col-12 col-sm-6 d-flex justify-content-center">
-                <img class="rounded-lg" src="https://dummyimage.com/600x400" alt="landing-page-image">
+            <div class="d-none col-12 col-md-6 d-md-flex justify-content-center">
+                <img class="rounded-lg img-fluid" src="https://dummyimage.com/600x400" alt="landing-page-image">
             </div>
         </div>
     </div>
