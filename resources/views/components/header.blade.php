faad1cd07a50508ef6d802fa623f4338bc7ddafb
Dendi Handian <handian.dendi@gmail.com>
2020-11-08 01:58:35 +0700

    add navbar-dark to make toggler visible

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,10 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-teal py-2 px-4  py-lg-0">
+    <nav class="navbar navbar-expand-md navbar-dark bg-teal py-2 px-4 py-md-0">
         <a class="navbar-brand text-white" href="#">
             <i class="fab fa-twitter"></i>&nbsp;Twina
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon text-white"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
